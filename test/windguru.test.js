0e6deb7b7281d82778bf67268707b11b40b9b527
astopo <amy.soetopo@gmail.com>
2020-12-31 19:43:16 +0100

    [test] fix scope

--- test/windguru.test.js
+++ test/windguru.test.js
@@ -7,21 +7,21 @@ describe('WindGuru', function() {
     it('should throw an error if the WINDGURU_USER environment variable does not exist', function() {
       const wg = new WindGuru()
 
-      expect(wg.init).to.throw(/WINDGURU_USER is required/)
+      expect(wg.init.bind(wg)).to.throw(/WINDGURU_USER is required/)
     })
 
     it('should throw an error if the WINDGURU_PASSWORD environment variable does not exist', function() {
       process.env.WINDGURU_USER = 'someuser'
       
       const wg = new WindGuru()
-      expect(wg.init).to.throw(/WINDGURU_PASSWORD is required/)
+      expect(wg.init.bind(wg)).to.throw(/WINDGURU_PASSWORD is required/)
     })
 
     it('should not throw an error if all required environment variables exist', function() {
       process.env.WINDGURU_PASSWORD = 'somepassword'
 
       const wg = new WindGuru()
-      expect(wg.init).to.not.throw()
+      expect(wg.init.bind(wg)).to.not.throw()
     })
   })
 })
